37b7fd9ddabc8e133f39af14cd2407de4032bf38
Douxiaobo <dxb_1020@icloud.com>
2022-09-07 20:08:33 +0800

    2022/9/7

---
+++ WeatherReportWithCShapeConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace WeatherReportWithCShapeConsole
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
+
+
