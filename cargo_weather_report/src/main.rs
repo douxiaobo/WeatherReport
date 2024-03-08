f103fbf288406901648360c7a8c407c5ac94feb1
douxiaobo <dxb_1020@icloud.com>
2024-03-08 09:36:29 +0800

    rust city

--- cargo_weather_report/src/main.rs
+++ cargo_weather_report/src/main.rs
@@ -40,16 +40,18 @@ fn main() {
     // let text=std::fs::read_to_string("./province.txt").unwrap();
     // println!("{}",text);
 
-    let mut vector:Vec<String>=Vec::new();
+    let mut vector_province:Vec<String>=Vec::new();
+    let mut vector_city:Vec<String>=Vec::new();
 
-    if let Ok(lines) = read_lines("province.txt") {
+
+    if let Ok(lines) = read_lines("../placename/province.txt") {
         // 对于每一行，进行处理
         for line in lines {
             // 再次使用 if let 语句处理每一行的读取结果
             if let Ok(data) = line {
                 // 打印每一行的内容
                 // println!("{}", data);
-                vector.push(data);
+                vector_province.push(data);
             }
         }
     } else {
@@ -57,12 +59,11 @@ fn main() {
         println!("Failed to read file");
     }
 
-    for i in &vector {
-        println!("{}",i);
-    }
+    // for item in &vector_province {
+    //     println!("{}",item);
+    // }
 
-    let mut province=String::new();
-    
+    let mut province=String::new();   
 
     print!("请输入省份：");
     loop{        
@@ -70,21 +71,52 @@ fn main() {
         std::io::stdout().flush().unwrap();// 确保提示信息立即显示
         std::io::stdin().read_line(&mut text).expect("failed to read line.");
 
-        if vector.contains(&text.trim_end().to_string()){
+        if vector_province.contains(&text.trim_end().to_string()){
             province=text.trim_end().to_string();
             println!("找到了省份：{}",province);
             break;
         } else {
-            print!("没有省份的名单，请重新输入：");
+            print!("对不起，没有省份的名单，请重新输入：");
         }        
     }
-    
+    let path=format!("../placename/{}.txt",province);
 
-    // let mut city=String::new();
-    
-    // print!("请输入城市：");
-    // std::io::stdout().flush().unwrap();// 确保提示信息立即显示  
-    // std::io::stdin().read_line(&mut city).expect("failed to read line.");
+    if let Ok(lines) = read_lines(path) {
+        // 对于每一行，进行处理
+        for line in lines {
+            // 再次使用 if let 语句处理每一行的读取结果
+            if let Ok(data) = line {
+                // 打印每一行的内容
+                // println!("{}", data);
+                vector_city.push(data);
+            }
+        }
+    } else {
+        // 如果读取文件失败，打印错误信息
+        println!("Failed to read file");
+    }
 
-    // println!("省份：{0}，城市：{1}。",province.trim(),city.trim());
+    // for item in &vector_city {
+    //     println!("{}",item);
+    // }
+
+
+    let mut city=String::new();
+    
+    print!("请输入城市：");
+    loop {
+        let mut text=String::new();
+        std::io::stdout().flush().unwrap();// 确保提示信息立即显示  
+        std::io::stdin().read_line(&mut text).expect("failed to read line.");
+
+        if vector_city.contains(&text.trim_end().to_string()){
+            city=text.trim_end().to_string();
+            println!("找到了城市：{}",city);
+            break;
+        } else {
+            print!("对不起，没有城市的名单，请重新输入：");
+        }
+    }
+    
+    println!("省份：{0}，城市：{1}。",province.trim(),city.trim());
 }
