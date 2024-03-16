5e674ebd59333e04453b31d2bf5843ebabc45cec
douxiaobo <dxb_1020@icloud.com>
2024-03-16 14:01:29 +0800

    delete some files

--- RestMethod.ps1
+++
@@ -1,2 +0,0 @@
-$response = Invoke-RestMethod 'https://wis.qq.com/weather/common?source=pc&city={city}&province={province}&weather_type=observe' -Method 'GET' -Headers $headers
-$response | ConvertTo-Json
